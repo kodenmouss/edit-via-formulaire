0082a94d1ed2f830c6d41447d0a417888fb3708e
kodenmouss <yoannbodier@hotmail.com>
2017-04-20 01:45:03 +0200

    Create temp3.php

---
+++ temp3.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta name="robots" content="noindex">
+  <title>%title%</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <meta name="keywords" content="%pagedomaine%">
+
+  <!-- titre page -->
+  <meta name="title" content="%titremenu%">
+  <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body>
+
+  <header>
+    <h1>BANIERE</h1>
+  </header>
+
+  <nav class="col-sm-2 nav">
+    <?php
+    include("index.php");
+    ?>
+  </nav>
+
+
+    <div class="col-sm-10 content">
+
+<h1>%titre%</h1>
+
+
+<!-- <a href="h"> site officiel </a> -->
+<!-- <code> un code de </code> -->
+<!-- <img src=".jpg" alt="texte" style="width:304px;height:228px;"> -->
+<!-- <ol>
+  <li> comme la </li>
+  <li> et la </li>
+</ol>  -->
+<!-- list num <ul> <li> ... </li> </ul>  -->
+
+<h2>%soustitre%</h2>
+
+
+<p>%content%</p>
+
+<img src="upload/%image%" alt=" " style="width:304px;height:228px;">
+
+  <h2>les etapes</h2>
+
+<p> blabla images des href
+</p>
+
+
+
+  <h3> un sous tire lege </h3>
+
+  <p> encore un blabla ou on peut avoir des code </p>
+
+</div>
+  </div>
+</div>
+</body>
+</html>
